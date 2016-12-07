671f7aac6d174e50a128b136d4fe7b9024d0a202
travispbuckley <travispbuckley@lewisu.edu>
2016-12-07 15:16:02 -0600

    add migrations for other 3 models

--- pear-board/db/migrate/20161207205709_create_slots.rb
+++ pear-board/db/migrate/20161207205709_create_slots.rb
@@ -1,8 +1,10 @@
 class CreateSlots < ActiveRecord::Migration[5.0]
   def change
     create_table :slots do |t|
+    	t.string :day, (null: false)
+    	t.string :start, (null: false)
 
-      t.timestamps
+      t.timestamps(null: false)
     end
   end
 end
