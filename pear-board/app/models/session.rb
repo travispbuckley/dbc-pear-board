b7e8245bcdcc385f236b2b6f4b3d54fda5267094
travispbuckley <travispbuckley@lewisu.edu>
2016-12-07 15:50:57 -0600

    add associations for database

--- pear-board/app/models/session.rb
+++ pear-board/app/models/session.rb
@@ -1,2 +1,6 @@
 class Session < ApplicationRecord
+	belongs_to :slot
+	belongs_to :student, { class_name: "User" }
+	belongs_to :mentor, { class_name: "User" }
+	has_many :feedbacks
 end
--- pear-board/app/models/user.rb
+++ pear-board/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+	has_many :feedbacks, { foreign_key: :author_id }
+	has_many :student_sessions, { foreign_key: :student_id, class_name: "Session"}
+	has_many :mentor_sessions, { foreign_key: :mentor_id, class_name: "Session" } 
+	has_many :student_slots, through: :student_sessions, source: :slot
+	has_many :mentor_slots, through: :mentor_sessions, source: :slot
 end
