62ba84a30b1b079f6e440c557b286bdbaf89ad91
travispbuckley <travispbuckley@lewisu.edu>
2016-12-07 15:21:29 -0600

    ran migrations to test for errors, errors fixed and migrations complete

--- pear-board/db/migrate/20161207205703_create_sessions.rb
+++ pear-board/db/migrate/20161207205703_create_sessions.rb
@@ -2,9 +2,9 @@ class CreateSessions < ActiveRecord::Migration[5.0]
   def change
     create_table :sessions do |t|
     	t.integer :student_id
-    	t.integer :mentor_id, (null: false)
-    	t.integer :slot_id, (null: false)
-    	t.datetime :session_datetime, (null: false)
+    	t.integer :mentor_id, { null: false }
+    	t.integer :slot_id, { null: false }
+    	t.datetime :session_datetime, { null: false }
 
 
       t.timestamps(null: false)
--- pear-board/db/migrate/20161207205709_create_slots.rb
+++ pear-board/db/migrate/20161207205709_create_slots.rb
@@ -1,8 +1,8 @@
 class CreateSlots < ActiveRecord::Migration[5.0]
   def change
     create_table :slots do |t|
-    	t.string :day, (null: false)
-    	t.string :start, (null: false)
+    	t.string :day, { null: false }
+    	t.string :start, { null: false }
 
       t.timestamps(null: false)
     end
