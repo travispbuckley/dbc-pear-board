78843fd4f14f862bc21f56f4586620bb8284120a
Collin Rea <email": null>
2016-12-08 14:50:26 -0600

    Completes session spec to check if datetime is on the weekend

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Session, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:session) {Session.new(slot_id: 1, student_id: 1, mentor_id: 2, session_datetime: DateTime.new(2016, 12, 11, 3, 30, 0))}
+
+  describe "attributes" do
+    context "test attributes of Session model" do
+      it 'has a datetime on the weekend' do
+        expect(session.session_datetime.on_weekend?).to be_truthy
+      end
+    end
+  end
 end
