b9c0b4b90b9cefd62cd9dbe151ad31a270009875
bradley simmons <bradleysimmons9@gmail.com>
2016-12-07 18:32:48 -0600

    add register and login links to users#index; add routes for registration, auth, logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
 
   root to: "users#index"
 
+  resources :users, {:except => :delete }
+
+  get "/register" => "users#new"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy", :via => :delete
+
+
 end
