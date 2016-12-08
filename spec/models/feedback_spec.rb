2ef82fd6ff559d2ab091d10b4ca910536f095aa9
Collin Rea <email": null>
2016-12-08 14:54:47 -0600

    Complete attribute unit tests

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Feedback, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:feedback) {Feedback.new(author_id: 1, session_id: 1, feedback_text: "Best mentor ever!")}
+
+  describe "attributes" do
+    context "test attributes of Feedback model" do
+      it 'feedback text' do
+        expect(feedback.feedback_text).to eq "Best mentor ever!"
+      end
+    end
+  end
 end
