20f0a42b863da2aacba4a4eabb9307393367c7d8
bradley simmons <bradleysimmons9@gmail.com>
2016-12-08 11:12:43 -0600

    write tests for invalid register input

--- spec/features/registers_user_feature_spec.rb
+++ spec/features/registers_user_feature_spec.rb
@@ -11,7 +11,7 @@ feature "registers a new user" do
     expect(page).to have_current_path register_path
   end
 
-  scenario "the user can register" do
+  scenario "user registers with valid information" do
     visit register_path
 
     within(".new_user") do
@@ -25,5 +25,21 @@ feature "registers a new user" do
 
     expect(page).to have_current_path root_path
     expect(page.find_by_id('profile_link')).to have_content("username_test")
+  end
+
+  scenario "user is shown errors with invalid information" do
+    visit register_path
+
+    within(".new_user") do
+      fill_in "user_full_name", with: ""
+      fill_in "user_email", with: ""
+      fill_in "user_phone_number", with: ""
+      fill_in "user_password", with: ""
+      fill_in "user_password_confirmation", with: ""
+      click_button("Register")
+    end
+
+    
+    expect(page.find_by_id('errors')).to have_content()
   end
 end
